e7b048ecf3122aabca75885f178b00af5299cf73
student5 <student3@jh-mba158-13.d214.org>
2017-03-20 10:07:55 -0500

    set up map view

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
