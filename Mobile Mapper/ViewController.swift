0e8ce8673e9f3551a686dce92e2c54a6a38f82d7
student5 <student3@jh-mba158-13.local>
2017-03-21 10:02:04 -0500

    clgeocoder

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -11,10 +11,12 @@ import MapKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    let herseyAnnotation = MKPointAnnotation()
+    let address = "Mount Rushmore"
+    let geocoder = CLGeocoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let herseyAnnotation = MKPointAnnotation()
         let latitude: Double = 42.102332924
         let longitude: Double = -87.955667844
         let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
@@ -22,6 +24,15 @@ class ViewController: UIViewController {
         herseyAnnotation.title = "Hersey High School"
         mapView.addAnnotation(herseyAnnotation)
         
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            for place in placemarks!
+            {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
 }
 
