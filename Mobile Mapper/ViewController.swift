1ae74798afa1270335a95a3ad589919371ed674d
student5 <student3@jh-mba158-13.d214.org>
2017-03-23 10:37:36 -0500

    finished

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
     let herseyAnnotation = MKPointAnnotation()
     let address = "Mount Rushmore"
     let geocoder = CLGeocoder()
+    let locationManager = CLLocationManager()
+    var initialRegion: MKCoordinateRegion!
+    var isInitialMapLoad: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +27,10 @@ class ViewController: UIViewController {
         herseyAnnotation.coordinate = coordinate
         herseyAnnotation.title = "Hersey High School"
         mapView.addAnnotation(herseyAnnotation)
+        mapView.delegate = self
         
         geocoder.geocodeAddressString(address) { (placemarks, error) in
+            //print(placemarks?.count)
             for place in placemarks!
             {
                 let annotation = MKPointAnnotation()
@@ -33,6 +39,56 @@ class ViewController: UIViewController {
                 self.mapView.addAnnotation(annotation)
             }
         }
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+    }
+    
+    @IBAction func whenButtonTapped(_ sender: UIBarButtonItem)
+    {
+        mapView.setRegion(initialRegion, animated: true)
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView)
+    {
+        if isInitialMapLoad
+        {
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialMapLoad = false
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        var pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        
+        if annotation.isEqual(mapView.userLocation)
+        {
+            return nil
+        }
+        
+        if annotation.isEqual(annotation)
+        {
+            pin.image = UIImage(named: "MobileMakersPin")
+        }
+            
+        else {
+            
+            pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        }
+
+        pin.image = UIImage(named: "logo")
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        return pin
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl)
+    {
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let region = MKCoordinateRegion(center: (view.annotation?.coordinate)!, span: span)
+        mapView.setRegion(region, animated: true)
     }
 }
 
